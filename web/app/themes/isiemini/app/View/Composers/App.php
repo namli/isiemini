c09f22cfb8ae2f8842665b32c7131f6e00f66706
Aleksey <aleksey.andruschenko@moravio.com>
2025-06-23 22:03:34 +0200

    Enhance theme structure and styling. Added navigation menu functionality in App.php, updated header layout with responsive design, and improved CSS styles for buttons and links. Adjusted page-header visibility and included additional theme details in style.css.

--- web/app/themes/isiemini/app/View/Composers/App.php
+++ web/app/themes/isiemini/app/View/Composers/App.php
@@ -16,10 +16,52 @@ class App extends Composer
     ];
 
     /**
+     * Data to be passed to view before rendering.
+     *
+     * @return array
+     */
+    public function with()
+    {
+        return [
+            'siteName' => $this->siteName(),
+            'primaryMenu' => $this->getNavMenuItems('primary_navigation'),
+        ];
+    }
+
+    /**
      * Retrieve the site name.
      */
     public function siteName(): string
     {
         return get_bloginfo('name', 'display');
+    }
+
+    /**
+     * Возвращает пункты меню в виде массива.
+     *
+     * @param string $location
+     * @return array|false
+     */
+    public function getNavMenuItems($location)
+    {
+        if (!has_nav_menu($location)) {
+            return false;
+        }
+
+        $locations = get_nav_menu_locations();
+        $menu = wp_get_nav_menu_object($locations[$location]);
+        $menu_items = wp_get_nav_menu_items($menu->term_id, ['order' => 'DESC']);
+
+        // Добавляем проверку на текущий активный пункт
+        // foreach ($menu_items as $key => $item) {
+        //     dd($item);
+        //     if (in_array('current-menu-item', $item->classes) || in_array('current-page-ancestor', $item->classes)) {
+        //         $menu_items[$key]->is_current = true;
+        //     } else {
+        //         $menu_items[$key]->is_current = false;
+        //     }
+        // }
+
+        return $menu_items;
     }
 }
--- web/app/themes/isiemini/resources/views/sections/header.blade.php
+++ web/app/themes/isiemini/resources/views/sections/header.blade.php
@@ -1,11 +1,67 @@
-<header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
-  </a>
+<header class="bg-white">
+  <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <a href="/" class="-m-1.5 p-1.5">
+      <span class="text-2xl font-bold">{{ $siteName }}</span>
+    </a>
+    <div class="flex lg:hidden">
+      <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+        <span class="sr-only">Open main menu</span>
+        <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </button>
+    </div>
+    <div class="hidden lg:flex lg:gap-x-12 justify-end">
+      <div class="flex gap-x-12">
+        @if ($primaryMenu)
+        @foreach ($primaryMenu as $item)
+        <a
+          href="{{ $item->url }}"
+          class="text-sm/6 font-semibold text-gray-900 no-underline flex items-center"
 
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
-  @endif
+          aria-current="{{ $item->is_current ? 'page' : 'false' }}">
+          {{ $item->title }}
+        </a>
+        @endforeach
+        @endif
+      </div>
+      <div class="shrink-0">
+        <a class="btn" href="#">
+          Записаться на консультацию
+        </a>
+      </div>
+    </div>
+
+
+  </nav>
+  <!-- Mobile menu, show/hide based on menu open state. -->
+  <div class="lg:hidden" role="dialog" aria-modal="true">
+    <!-- Background backdrop, show/hide based on slide-over state. -->
+    <div class="fixed inset-0 z-50"></div>
+    <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+      <div class="flex items-center justify-between">
+        <a href="/" class="-m-1.5 p-1.5">
+          <span class="sr-only">{{ $siteName }}</span>
+          <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+        </a>
+        <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+          <span class="sr-only">Close menu</span>
+          <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+      <div class="mt-6 flow-root">
+        <div class="-my-6 divide-y divide-gray-500/10">
+          <div class="space-y-2 py-6">
+            @if ($primaryMenu)
+            @foreach ($primaryMenu as $item)
+            <a href="{{ $item->url }}" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">{{ $item->title }}</a>
+            @endforeach
+            @endif
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </header>
